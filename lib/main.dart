04c1a476ed64475c1a42f425e78552f1d9375869
Donquixot228 <64096633+Donquixot228@users.noreply.github.com>
2022-08-03 13:01:38 +0300

    example

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flame_trex/trex_game.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    GameWidget(game: TRexGame()),
+  );
+}
